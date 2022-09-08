18c16e1baba80436b57a546a363c7880e1fd0a8b
Pablo Li <76201903+EfficientBogoSort@users.noreply.github.com>
2022-09-08 14:31:26 +1000

    Implemented the getMe endpoint and fixed some comments

--- users/urls.py
+++ users/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('signup/', views.RegisterView.as_view()),
     path('login/', views.LogInView.as_view()),
+    path('getMe/', views.get_user),
 ]
--- users/views.py
+++ users/views.py
@@ -2,7 +2,10 @@ from rest_framework.response import Response
 from .serializers import UserSerializer, LogInSerializer
 from rest_framework.views import APIView
 from .models import User
-
+from rest_framework.decorators import api_view
+import jwt
+from django.conf import settings
+from datetime import datetime
 # Create your views here.
 
 TOKEN_DURATION = 5
@@ -29,10 +32,26 @@ class LogInView(APIView):
         username = request.data.get('username', None)
         password = request.data.get('password', None)
 
-        # find a user with a matching username and check if the password is correct
+        # verify user exists
         user = User.objects.filter(username=username).first()
         if user is None or not user.check_password(password):
             return Response(status=401)
-        # the user authenticated, generate a JWT token for the user, which is done by the serializer
-        serializer = LogInSerializer(user)
-        return Response(serializer.data, status=200)
+        # the serializer craetes a JWT token for the user to use
+        user_serialized_data = LogInSerializer(user)
+        return Response(user_serialized_data.data, status=200)
+
+@api_view(['POST'])
+def get_user(request):
+    token = request.data.get('authToken')
+    # returns 404 for when the token is not in the request, the username is not in the token, the token expired
+    # or the user is not in the database
+    if not token:
+        return Response(status=404)
+    decoded_token = jwt.decode(token, settings.SECRET_KEY, algorithms='HS256')
+    if 'username' not in decoded_token or decoded_token['exp'] > datetime.utcnow():
+        return Response(404)
+    user = User.objects.filter(username=decoded_token['username']).first()
+    if user is None:
+        return Response(status=404)
+    user_serialized_data = UserSerializer(user)
+    return Response(user_serialized_data.data, status=200)
