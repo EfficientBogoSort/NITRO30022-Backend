416d32cb562bb0f7d0f48e3c6d29451113037a02
Pablo Li <76201903+EfficientBogoSort@users.noreply.github.com>
2022-10-17 22:37:44 +1100

    Added list and retrieve endpoints for Friends and added an update endpoint for users. It only works with username for now

--- friends/views.py
+++ friends/views.py
@@ -3,6 +3,9 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from collection.views import get_token, decode_token
 from users.__init__ import *
+from serializers import FriendSerializer
+
+
 # Create your views here.
 
 class FriendViewSet(viewsets.ModelViewSet):
@@ -22,6 +25,7 @@ class FriendViewSet(viewsets.ModelViewSet):
         user.friends.add(friend)
         friend.friends.add(user)
         return Response(status=OK_STAT_CODE)
+
     def destroy(self, request, pk):
         token = decode_token(get_token(request))
         if token == INVALID_DATA_CODE or token == BAD_REQ_CODE:
@@ -38,3 +42,26 @@ class FriendViewSet(viewsets.ModelViewSet):
         user.friends.remove(friend)
         friend.friends.remove(user)
         return Response(status=OK_STAT_CODE)
+
+    def list(self, request):
+        token = decode_token(get_token(request))
+        if token == INVALID_DATA_CODE or token == BAD_REQ_CODE:
+            return Response(status=token)
+        username = token.get('username', None)
+        user = User.objects.filter(username=username).first()
+        if user is None:
+            return Response(status=NOT_FOUND, data={'message': 'User not found'})
+        serialized_friends = FriendSerializer(user)
+        return Response(serialized_friends.data, status=OK_STAT_CODE)
+
+    def retrieve(self, request, pk):
+        token = decode_token(get_token(request))
+        if token == INVALID_DATA_CODE or token == BAD_REQ_CODE:
+            return Response(status=token)
+        username = token.get('username', None)
+        user = User.objects.filter(username=username).first()
+        if user is None:
+            return Response(status=NOT_FOUND, data={'message': 'User not found'})
+        friend = User.objects.filter(username=pk).first()
+        if friend is None:
+            return Response(status=NOT_FOUND, data={'message': 'Friend not found'})
