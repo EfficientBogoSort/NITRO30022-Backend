541d11f2b2ea728ed3462e113f58e90b74d5bed6
surya-ven <84949099+surya-ven@users.noreply.github.com>
2022-10-23 19:05:33 +1100

    clean prod for merge

--- collection/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from .__init__ import *
-from rest_framework.routers import SimpleRouter
-from .views import CollectionViewSet
-router = SimpleRouter()
-router.register('', CollectionViewSet, basename=COLLECTION_URL)
-
-urlpatterns = router.urls
--- friends/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from users.models import User
-
-class FriendSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['friends']
