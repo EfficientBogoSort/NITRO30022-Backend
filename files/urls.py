880bcf6741313ff6254e2629eca56579e2541ac5
surya-ven <84949099+surya-ven@users.noreply.github.com>
2022-10-23 18:44:06 +1100

    Cleaned prod for merge

--- files/urls.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.routers import SimpleRouter
-from .views import FileViewset
-
-router = SimpleRouter()
-router.register('accounts', FileViewset)
-urlpatterns = router.urls
--- users/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('signup/', views.RegisterView.as_view()),
-    path('login/', views.LogInView.as_view()),
-]
