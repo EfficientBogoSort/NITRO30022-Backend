4e5205ffc9a81d5c8d3f79791a268b93e199e328
Pablo Li <76201903+EfficientBogoSort@users.noreply.github.com>
2022-11-09 19:13:43 +1100

    Fixed other typos

--- files/views.py
+++ files/views.py
@@ -42,7 +42,7 @@ class FileViewset(viewsets.ModelViewSet):
         if user is None:
             return Response(status=INVALID_DATA_CODE, data={'message': 'User does not exist'})
         
-        file = File.objects.filter(id=pk, owner=user.username).first()
+        file = File.objects.filter(name=pk, owner=user.username).first()
         # print("file", file)
         if file is None:
             return Response(status=NOT_FOUND)
@@ -121,7 +121,7 @@ class FileViewset(viewsets.ModelViewSet):
         if user is None:
             return Response(status=INVALID_DATA_CODE, data={'message': 'User does not exist'})
         
-        file = File.objects.filter(id=pk, owner=user.username).first()
+        file = File.objects.filter(title=pk, owner=user.username).first()
 
         new_title = request.data.get('title')
         if file is None:
